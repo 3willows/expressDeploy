05701a4d40e8b85c55c9b1ce89d16ccadc499f36
Jasper Wong <wlcjasper@gmail.com>
2024-06-21 15:06:06 +0800

    comment out app.listen for deployment

--- api/index.ts
+++ api/index.ts
@@ -7,9 +7,10 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-	res.send("hi");
+	res.send("hello I am a deployed express App!");
 });
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+// need to remove this line when deploying
+// app.listen(3000, () => console.log('Server ready on port 3000.'));
 
 module.exports = app;
