fbe31f5793c9625c320b21edd8421c3116d61d17
Jasper Wong <wlcjasper@gmail.com>
2024-06-28 22:19:32 +0800

    Connect Mongoose to the main express app.

--- api/index.js
+++ api/index.js
@@ -1,16 +1,54 @@
+require("dotenv").config()
 
-require('dotenv').config();
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
 
-const express = require('express');
-const app = express();
+app.use(express.static("public"))
 
-app.use(express.static('public'));
+main().catch((err) => console.log(err))
 
-app.get('/', function (req, res) {
-	res.send("hello I am a deployed express App!");
-});
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test")
+}
+
+const kittySchema = new mongoose.Schema({
+  name: String,
+})
+
+kittySchema.methods.speak = function speak() {
+  const greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name"
+  console.log(greeting)
+}
+
+const Kitten = mongoose.model("Kitten", kittySchema)
+
+const silence = new Kitten({ name: "Silence" })
+console.log(silence.name) // 'Silence'
+
+const fluffy = new Kitten({ name: "Blair" })
+fluffy.speak() // "Meow name is fluffy"
+
+async function doSomething() {
+  await fluffy.save()
+}
+doSomething()
+
+let numOfEntries;
+
+Kitten.countDocuments().then((count) => {
+	numOfEntries = count
+	console.log(count)})
+
+app.get("/", function (req, res) {
+  res.send(
+    `Hello!  I am a deployed express App!  Hello to ${fluffy.name}!  There are ${numOfEntries} in the Mongo Database.`
+  )
+})
 
 // need to remove this line when deploying
-// app.listen(3000, () => console.log('Server ready on port 3000.'));
+app.listen(3000, () => console.log("Server ready on port 3000."))
 
-module.exports = app;
+module.exports = app
