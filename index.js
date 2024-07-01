d83d87621e358acac49ce0e030a74f38d3c80ed1
Jasper Wong <wlcjasper@gmail.com>
2024-07-02 00:15:32 +0800

    Add GUI for inputting new documents to database

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ require("dotenv").config()
 const port = process.env.PORT
 
 const express = require("express")
+const bodyParser = require('body-parser');
 const app = express()
 
 app.use(express.static("public"))
 app.use(express.json())
+app.set('view engine', 'pug')
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => console.log(`Server ready on port ${port}.`))
 
@@ -54,12 +58,13 @@ async function findEveryone() {
 let postRequestName
 
 app.post("/", (req, res) => {
+  console.log(req.body)
   postRequestName = req.body.name
   addName()
 })
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   const judges = await findEveryone()
   const namesArray = judges.map(obj => obj.name)
-  res.send(`This everyone on the list: ${namesArray.join(", ")}!`)
+  res.render('index', { title: 'This is the title', judges: namesArray })
 })
